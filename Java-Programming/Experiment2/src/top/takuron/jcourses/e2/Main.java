b9248ef7ac9baec52b1fe97164c5a26a671f20c3
takuron <zhongsunxinghe@foxmail.com>
2022-03-03 09:31:49 +0800

    修改

--- Java-Programming/Experiment2/src/top/takuron/jcourses/e2/Main.java
+++ Java-Programming/Experiment2/src/top/takuron/jcourses/e2/Main.java
@@ -6,6 +6,7 @@ public class Main {
 
     public static void main(String[] args) {
 	    int answer = (int) ((Math.random()*100)+0.5);
+        int time = 0;
         System.out.println("=========================");
         System.out.println("=====欢迎进入猜数字游戏=====");
         System.out.println("=========================");
@@ -16,9 +17,23 @@ public class Main {
             Scanner sc = new Scanner(System.in);
             int input = sc.nextInt();
 
-            if(input==answer) break;
-            else if(input>answer) System.out.println("抱歉，你猜的太大了");
-            else if(input<answer) System.out.println("抱歉，你猜的太小了");
+            if(input==answer) {
+                System.out.println("恭喜猜对，共猜"+time+"次");
+                answer  = (int) ((Math.random()*100)+0.5);
+                time = 0;
+
+                System.out.print("是否继续？（Y/N）");
+                Scanner sc2 = new Scanner(System.in);
+                if(!sc2.next().equals("Y")) break;
+            }
+            else if(input>answer) {
+                System.out.println("抱歉，你猜的太大了");
+                time++;
+            }
+            else if(input<answer){
+                System.out.println("抱歉，你猜的太小了");
+                time++;
+            }
         }
 
         System.out.println("=========================");
