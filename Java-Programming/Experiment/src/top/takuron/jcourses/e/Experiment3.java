37342e8517823722828f12a9b8760673cfdfb774
takuron <zhongsunxinghe@foxmail.com>
2022-03-17 08:48:32 +0800

    实验3

---
+++ Java-Programming/Experiment/src/top/takuron/jcourses/e/Experiment3.java
@@ -0,0 +1,49 @@
+package top.takuron.jcourses.e;
+
+import java.io.*;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class Experiment3 {
+
+    private static Map<String,String> wordsMap;
+
+    public static void main(String[] args) {
+        try {
+            FileReader inFile = new FileReader("words.txt");
+            BufferedReader bf = new BufferedReader(inFile);
+            String lines = bf.readLine();
+            wordsMap = new HashMap();
+
+            while(lines!=null){
+                String word = lines.split("\t")[0];
+                wordsMap.put(word,lines);
+
+                lines = bf.readLine();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("=========================");
+        System.out.println("========欢迎进入词典=======");
+        System.out.println("=========================");
+
+        while (true){
+            System.out.print("请输入需要查询的单词(回车退出）：");
+            Scanner sc = new Scanner(System.in);
+
+            String word = sc.nextLine();
+            if(word.equals("")) break;
+            else if(wordsMap.get(word)==null){
+                System.out.println("非常抱歉，没找到单词"+word);
+            }
+            else{
+                System.out.println(wordsMap.get(word));
+            }
+
+        }
+
+    }
+}
